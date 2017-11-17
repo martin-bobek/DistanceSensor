ea6487939cfc9ad277b3d28a2a142b3b05e0b969
Martin Bobek <martin.bobek@ucalgary.ca>
2017-11-17 12:46:47 -0700

    Working and tested ~11 bit adc.

--- sources_1/new/adc.vhd
+++ sources_1/new/adc.vhd
@@ -8,7 +8,7 @@ entity adc is
         reset: in std_logic;
         sample: in std_logic;
         feedback: out std_logic;
-        output: out std_logic_vector(11 downto 0);
+        led: out std_logic_vector(11 downto 0)
     );
 end;
 
@@ -17,23 +17,43 @@ architecture behavioural of adc is
     
     signal counter: unsigned(14 downto 0);
     signal sample_num, u_output: unsigned(11 downto 0);
+    
+    signal tick, i_feedback: std_logic;
 begin
+    feedback <= i_feedback;
+
     process(clk, reset) begin
         if (reset = '1') then
             counter <= top;
-            feedback <= '0';
-            sample_num <= (others => '0');
+            i_feedback <= '0';
+            tick <= '0';
         elsif rising_edge(clk) then
             if (counter = top) then
                 counter <= (others => '0');
-                feedback <= sample;
-                sample_num <= sample_num;
-                
+                i_feedback <= sample;
+                tick <= '1';
             else
                 counter <= counter + 1;
+            end if;
+            
+            if (tick = '1') then
+                tick <= '0';
             end if;
         end if;
     end process;
     
-    
+    process(clk, reset) begin
+        if (reset = '1') then
+            sample_num <= (others => '0');
+        elsif rising_edge(clk) and (tick = '1') then
+            if (sample_num = 0) then
+                led <= std_logic_vector(u_output);
+                u_output <= "00000000000" & i_feedback;
+            elsif (i_feedback = '1') then
+                u_output <= u_output + 1;
+            end if;
+            
+            sample_num <= sample_num + 1;
+        end if;
+    end process;
 end;
